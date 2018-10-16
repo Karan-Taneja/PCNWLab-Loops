afe08dd1e8668788d2eb806656e9176548dad579
Karan Taneja <karantaneja@pursuit.org>
2018-10-16 09:04:41 -0400

    Finished pset1

--- pset1.js
+++ pset1.js
@@ -7,6 +7,20 @@
     @example - average([1,2,3,4]); // 2.5
 */
 
+const average = array => {
+
+    let total = 0
+
+    for (i = 0; i < array.length; i++){
+        total += array[i]
+    }
+
+    return total / array.length
+}
+
+console.log(average([1, 2, 3, 4]))
+console.log(average([11, 2, 14, 37, 9]))
+
 /*
     @func squareEach
     @param {array} arr
@@ -14,6 +28,19 @@
     @desc - square each value in array
     @example - squareEach([1,2,3,4]); // [1,4,9,16]
 */
+
+const squareEach = array => {
+
+    const nuArray = array
+
+    for (i = 0; i < array.length; i++){
+        nuArray[i] = nuArray[i] ** 2
+    }
+
+    return nuArray
+}
+
+console.log(squareEach([1, 2, 3, 4]))
 
 /*
     @func averageSquare
@@ -29,6 +56,17 @@
         averageSquare([1,2,3,4); // 7.5
 */
 
+
+const averageSquare = array => {
+
+    const nuArray = average(squareEach(array))
+    
+    return nuArray
+
+}
+
+console.log(averageSquare([1, 2, 3, 4]))
+
 /*
     @func negateArr
     @param {array} arr
@@ -38,6 +76,20 @@
         negateArr([1,2,3,4]); // [-1,-2,-3,-4]
 */
 
+const negateArr = array => {
+
+    const nuArray = array
+
+    for (i = 0; i < nuArray.length; i++){
+        nuArray[i] = nuArray[i] * -1
+    }
+
+    return nuArray
+
+}
+
+console.log(negateArr([1,2,3,4]))
+
 /*
     @func reverseArr
     @param {array} arr
@@ -45,6 +97,20 @@
     @desc - reverse an array
     @example - reverseArr([1,2,3,4]); // [4,3,2,1]
 */
+
+const reverseArr = array => {
+
+    const nuArray = []
+
+    for (i = array.length -1; i >= 0; i--){
+        nuArray.push(array[i])
+    }
+
+    return nuArray
+
+}
+
+console.log(reverseArr([1,2,3,4]))
 
 /*
     @func negateBackwards
@@ -55,5 +121,12 @@
         negateBackwards([1,2,3,4]); // [-4, -3, -2, -1]
 */
 
+const negateBackwards = array => {
 
+    let nuArray = reverseArr(negateArr(array))
 
+    return nuArray
+
+}
+
+console.log(negateBackwards([1,2,3,4]))
